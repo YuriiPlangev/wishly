26a5b847d226543623f560a8b1ac8497b7470995
Yurii Planhev <planghevyurii@gmail.com>
2025-06-03 11:57:34 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'content.rozetka.com.ua', 'content1.rozetka.com.ua' ],
   },
-<<<<<<< HEAD
-   eslint: {
-    ignoreDuringBuilds: true,
-=======
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
->>>>>>> my-hotfix
   },
 };
 
